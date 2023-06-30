8dbe8bc0bba2eb9ab2e32272afc6d8c00fb7d7bf
DIA24 <5084kkm@naver.com>
2023-06-30 23:58:53 +0900

    add Character, Item, WaterBalloon Class

---
+++ Assets/Script/Character.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character
+{
+    public float moveSpeed;
+    public int waterBalloonPower;
+    public int waterBalloonMaxCount;
+
+}
