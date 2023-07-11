0cc503c411aa2cdc8d88fb84452d4bd41477060a
DIA24 <5084kkm@naver.com>
2023-07-12 03:15:43 +0900

    fix item interface error

--- Assets/Script/Item/IItem.cs
+++ Assets/Script/Item/IItem.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IItem, IWallBoom 
+public interface IItem : IWallBoom 
 {
     void Get(Character Player);
 }
