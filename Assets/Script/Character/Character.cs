7cd241fe841ca402b755a2a4bfd626b4d9d45323
hayeon7898 <hayeon22@korea.ac.kr>
2023-07-13 17:47:25 +0900

    거북이 아이템 2차 코딩

--- Assets/Script/Character/Character.cs
+++ Assets/Script/Character/Character.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class Character : MonoBehaviour
@@ -10,6 +11,8 @@ public class Character : MonoBehaviour
     public int waterBalloonMaxCount = 1;
     private int currentWaterBalloons = 0;
     public float moveSpeed = 5f;
+    public bool isTurtleItem = false; //캐릭터의 거북이 아이템 유무 여부
+    public bool WaterBalloonHit = false; //캐릭터가 물풍선에 맞음 여부
 
     void Update()
     {
@@ -46,6 +49,44 @@ public class Character : MonoBehaviour
     public void WaterBalloonExploded()
     {
         currentWaterBalloons--;
+    }
+
+    //캐릭터에 거북이 아이템 효과 적용 함수
+    public void ApplyTurtleItemEffects(TurtleSpeed speed)
+    {
+        isTurtleItem = true;
+
+        if (speed == TurtleSpeed.Fast)
+        {
+            // 빠른 거북이 아이템의 효과를 적용
+        }
+        else
+        {
+            // 느린 거북이 아이템의 효과를 적용
+        }
+    }
+
+    //캐릭터가 물풍선에 맞은 경우를 구현한 함수
+    public void HitByWaterBalloon(bool WaterBalloonHit)
+    {
+        //물풍선에 맞은 경우
+        if (WaterBalloonHit)
+        {
+            //거북이를 탄 경우
+            if (isTurtleItem)
+            {
+
+            }
+
+            //바늘 아이템을 가진 경우
+
+            else
+            {
+                //캐릭터 삭제
+                //질문: 일정 시간이 경과후 캐릭터가 삭제되도록 할까요??
+                Destroy(this.gameObject);
+            }
+        }
     }
 
 
--- Assets/Script/Item/TurtleItem.cs
+++ Assets/Script/Item/TurtleItem.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
+//거북이 아이템 속도를 나타내는 열거형(enum)
 public enum TurtleSpeed
 {
     Fast,
     Slow
 }
 
-public class TurtleItem : IItem
+public class TurtleItem : MonoBehaviour,IItem
 {
-    private bool isDefending;
-    private TurtleSpeed speed;
+    private bool isDefending; // 거북이 아이템의 방어 기능
+    private TurtleSpeed speed; //거북이 아이템 스피드
 
-    private TurtleSpeed GetRandomSpeed()//거북이 아이템의 속도를 결정하는 랜덤 함수
+    //거북이 아이템의 속도를 결정하는 랜덤 함수
+    private TurtleSpeed GetRandomSpeed()
     {
         System.Random r = new System.Random();
         int randomValue = Random.Range(0, 10);
@@ -21,18 +24,28 @@ public class TurtleItem : IItem
         return randomValue < 2 ? TurtleSpeed.Fast : TurtleSpeed.Slow;
     } 
 
-    public void Get(Character player)
+    //캐릭터가 거북이 아이템을 획득한 경우
+    public void Get(Character Player)
     {
-        if (speed == TurtleSpeed.Fast)
-        {
-            //캐릭터가 빠른 거북이 아이템을 획득
-        }
-        else
-        {
-            //캐릭터가 느린 거북이 아이템을 획득
-        }
+        Player.ApplyTurtleItemEffects(speed);
     }   
 
-    //물풍선 맞을 때 함수 구현해야함
+    //거북이 아이템이 사라지는 경우
+    public void Defend(Character Player)
+    {
+        while (isDefending)
+        {
+            //캐릭터가 물풍선에 맞았을 때
+            if (Player.WaterBalloonHit)
+            {
+               //1회 방어 기능
+                Player.WaterBalloonHit = false;
+                isDefending = false;
+                
+            }
+        }
+
+        Destroy(this.gameObject);
+    }
 
 }
