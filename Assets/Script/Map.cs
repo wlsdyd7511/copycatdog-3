ccaf7bc4b3d8f16de14303bee0604315dab0a0b4
DIA24 <5084kkm@naver.com>
2023-07-23 23:13:09 +0900

    read maparr by txt file

--- Assets/Script/Map.cs
+++ Assets/Script/Map.cs
@@ -1,16 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class Map : MonoBehaviour
 {
     public GameObject[] walls = new GameObject[2];// 0 = 부숴지지 않는벽, 1 = 부숴지는벽
-    public GameObject[,] mapObject = new GameObject[15, 15];
-    public int[,] mapArr; // 0 = 빈공간, 1 = 부숴지지 않는벽, 2 = 부숴지는벽
+    public GameObject[,] mapObject;
+    public int[,] mapArr; // 0 = 빈공간, 1 = 부숴지지 않는벽, 2 = 부숴지는벽, 3 = 물풍선
+    private List<string> mapList = new List<string>();
     Vector3 wallPos;
     void Start()
     {
-        CreateMap(15);
+        DirectoryInfo di = new DirectoryInfo(Application.dataPath + "/Map");
+
+        foreach (FileInfo file in di.GetFiles("*.txt"))
+        {
+            
+            Debug.Log("파일명 : " + file.FullName);
+            mapList.Add(file.FullName);
+        }
+        CreateMap();
+        
     }
 
     // Update is called once per frame
@@ -19,26 +30,9 @@ public class Map : MonoBehaviour
         
     }
 
-    void CreateMap(int mapSize)
+    void CreateMap()
     {
-        mapArr = new int[15,15] 
-        { 
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0 },
-            { 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 2, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
-        };
+        ReadMapFile();
         for (int i = 0; i < mapArr.GetLength(0); i++)
         {
             for (int j = 0; j < mapArr.GetLength(1); j++)
@@ -50,5 +44,29 @@ public class Map : MonoBehaviour
                 }
             }
         }
+    }
+
+   void ReadMapFile()
+    {
+        
+        string[] contents = System.IO.File.ReadAllLines(mapList[Random.Range(0,mapList.Count)]);
+        Debug.Log(contents.Length);
+        mapArr = new int[contents.Length, contents.Length];
+        mapObject = new GameObject[contents.Length, contents.Length];
+        int cnt = 0;
+        if (contents.Length > 0)
+        {
+            for (int i = 0; i < contents.Length; i++)
+            {
+                string[] txtArr = contents[i].Split(',');
+
+                int[] numArr = new int[txtArr.Length];
+                for (int j = 0; j < numArr.Length; j++)
+                {
+                    mapArr[cnt,j] = int.Parse(txtArr[j]);
+                }
+                cnt++;
+            }
+        } 
     }
 }
