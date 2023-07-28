7d73f8403aaa5744a34b3b9142d37ad68b282d7d
hayeon7898 <hayeon22@korea.ac.kr>
2023-07-29 03:38:54 +0900

    Ufo 탑승 구현

--- Assets/Script/Item/UfoItem.cs
+++ Assets/Script/Item/UfoItem.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class UfoItem : MonoBehaviour, IItem
 {
+
+    [SerializeField]
+    private GameObject RideUfoItem;
+    
     //캐릭터가 Ufo 아이템을 획득한 경우
     public void Get(Character Player)
     {
         Player.ApplyUfoItemEffects();
+
+        //탑승 처리
+        RideUfoItem rideUfoItem = Instantiate(RideUfoItem).GetComponent<RideUfoItem>();
+        rideUfoItem.Ride(Player);
+
         Destroy(this.gameObject);
     }
     void OnTriggerEnter2D(Collider2D obj)
