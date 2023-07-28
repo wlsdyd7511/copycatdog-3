e0e35e8305c393132aca8b1c69986dde516e75ff
go00woo <go00woo@korea.ac.kr>
2023-07-29 03:06:35 +0900

    인게임씬 전환 기능 구현

---
+++ Assets/StartInGame.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//SceneManagement
+using UnityEngine.SceneManagement;
+
+public class StartInGame : MonoBehaviour
+{
+    public void StartButton()
+    {
+        SceneManager.LoadScene("InGameScene");  //대기실 화면으로 전환
+    }
+}
