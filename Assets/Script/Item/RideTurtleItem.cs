e39f267149a07bd90fdbf3574b0b1140835e3527
hayeon7898 <hayeon22@korea.ac.kr>
2023-07-29 06:47:56 +0900

    거북이 방향 가져오기2

--- Assets/Script/Item/RideTurtleItem.cs
+++ Assets/Script/Item/RideTurtleItem.cs
@@ -6,17 +6,13 @@ using UnityEngine;
 
 public class RideTurtleItem : MonoBehaviour, IRideable
 {
-    private Character rider;
-    private Character.Direction riderDirection;
+    private Character rider;  
 
     public void Ride(Character player)
     {
         // 캐릭터를 탑승 처리하고, 캐릭터 속도를 조정
         rider = player;
-        player.ApplyRideableItem(this);        
-
-        // 거북이 아이템의 방향을 캐릭터 방향으로 설정
-        riderDirection = rider.playerDir;
+        rider.ApplyRideableItem(this);        
 
         // 거북이 아이템을 캐릭터의 자식으로 설정
         transform.SetParent(player.transform);
@@ -25,6 +21,21 @@ public class RideTurtleItem : MonoBehaviour, IRideable
         Vector3 offset = new Vector3(0f, -0.5f, 0f); // 예시로 아래로 0.5만큼 내려가도록 설정
         transform.localPosition = offset;
 
+    }
+
+    private void LateUpdate()
+    {
+        // 거북이를 탄 경우, 캐릭터와 같은 방향으로 회전
+        if (rider != null)
+        {
+            // 캐릭터의 방향을 읽어와서 거북이 방향을 업데이트
+            transform.rotation = Quaternion.Euler(0, 0, -(int)rider.playerDir * 90);
+        }
+    }
+
+    public void Dismount()
+    {
+        rider = null;
     }
 
 }
